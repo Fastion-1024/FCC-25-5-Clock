f5f4e0930a7289ab7f3246aa27027d4a1fafb681
Fastion-1024 <dsprice1989@outlook.com>
2021-09-06 16:41:04 +0100

    Added Analogue Clock Style - Added Analogue Clock Component. - Added Analogue Clock Stylesheet

---
+++ src/components/AnalogueClock/AnalogueClock.js
@@ -0,0 +1,36 @@
+import './AnalogueClock.css';
+
+export const AnalogueClock = ({ time }) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+
+    const secondHandDeg = (seconds / 60) * 360;
+    // const minuteHandDeg = (minutes / 60) * 360 + (seconds / 60) * 6;
+    const minuteHandDeg = (minutes / 60) * 360;
+
+    console.log(secondHandDeg);
+    console.log(minuteHandDeg);
+
+    return (
+        <div className='clock-container'>
+            <div className='analogue-clock'>
+                <div className='outer-face'>
+                    <span className='mark mark-one' />
+                    <span className='mark mark-two' />
+                    <span className='mark mark-three' />
+                    <span className='mark mark-four' />
+                    <div className='inner-face'>
+                        <span
+                            className='hand second-hand'
+                            style={{ transform: `rotate(${secondHandDeg}deg)` }}
+                        />
+                        <span
+                            className='hand minute-hand'
+                            style={{ transform: `rotate(${minuteHandDeg}deg)` }}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
