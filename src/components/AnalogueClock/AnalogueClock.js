8c5bf553e5ff345c5199be55a6a4bdc292720354
Fastion-1024 <dsprice1989@outlook.com>
2021-09-20 11:31:29 +0100

    Analogue Clock Now Dynamically Resizes - Added custom hook useResizeObserver.  - Added div as a container for layout, centering etc.  - Parent container is monitored for resize changes. - Clock will now adjust height and width to be the smaller of its parent container. - Clock will now keep a 1:1 ratio.

--- src/components/AnalogueClock/AnalogueClock.js
+++ src/components/AnalogueClock/AnalogueClock.js
@@ -1,6 +1,10 @@
 import './AnalogueClock.css';
+import useResizeObserver from '../../hooks/useResizeObserver';
 
 const AnalogueClock = ({ time }) => {
+    const [containerRef, dimensions] = useResizeObserver();
+    const size = dimensions.width < dimensions.height ? dimensions.width : dimensions.height;
+
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
 
@@ -8,25 +12,24 @@ const AnalogueClock = ({ time }) => {
     // const minuteHandDeg = (minutes / 60) * 360 + (seconds / 60) * 6;
     const minuteHandDeg = (minutes / 60) * 360;
 
-    console.log(secondHandDeg);
-    console.log(minuteHandDeg);
-
     return (
-        <div className='analogue-clock'>
-            <div className='outer-face'>
-                <span className='mark mark-one' />
-                <span className='mark mark-two' />
-                <span className='mark mark-three' />
-                <span className='mark mark-four' />
-                <div className='inner-face'>
-                    <span
-                        className='hand second-hand'
-                        style={{ transform: `rotate(${secondHandDeg}deg)` }}
-                    />
-                    <span
-                        className='hand minute-hand'
-                        style={{ transform: `rotate(${minuteHandDeg}deg)` }}
-                    />
+        <div className='analogue-clock-container' ref={containerRef}>
+            <div className='analogue-clock' style={{ width: `${size}px`, height: `${size}px` }}>
+                <div className='outer-face'>
+                    <span className='mark mark-one' />
+                    <span className='mark mark-two' />
+                    <span className='mark mark-three' />
+                    <span className='mark mark-four' />
+                    <div className='inner-face'>
+                        <span
+                            className='hand second-hand'
+                            style={{ transform: `rotate(${secondHandDeg}deg)` }}
+                        />
+                        <span
+                            className='hand minute-hand'
+                            style={{ transform: `rotate(${minuteHandDeg}deg)` }}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
